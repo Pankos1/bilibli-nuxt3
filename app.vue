f8acfedeacedd49b099c45396952167c26a3c105
庞聪 <1968784036@qq.com>
2024-09-26 16:34:32 +0800

    初始化，使用vant组件库

---
+++ app.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+// seo优化
+useSeoMeta({
+  title: "统计鸟专业网站统计",
+  // 介绍
+  description: "统计鸟国内专业网站统计",
+  // 关键飞
+  keywords: "网站统计,tongjiniao, "
+})
+</script>
+
+<template>
+
+  <!-- routerview -->
+  <NuxtPage></NuxtPage>
+
+
+  <!-- routerlink
+  <nuxt-link to="/">首页</nuxt-link> -->
+</template>
+
+
+<style lang="scss">
+:root {
+  --van-primary-color: #FB7299 !important;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  // 是否开启调试工具
+  devtools: { enabled: true },
+  //是否开启ssr服务器渲染
+  ssr:true,
+  modules:['@vant/nuxt']
+})
